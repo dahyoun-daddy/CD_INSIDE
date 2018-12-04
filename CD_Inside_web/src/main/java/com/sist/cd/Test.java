7bb51364fc3ecfffa9be83c98df9a0fe3866b941
sist <sist@sist-Samsung-DeskTop-System>
2018-12-04 19:15:28 +0900

    이거두개만 테스트 두개만

--- CD_Inside_web/src/main/java/com/sist/cd/Test.java
+++ CD_Inside_web/src/main/java/com/sist/cd/Test.java
@@ -3,7 +3,7 @@ package com.sist.cd;
 public class Test {
 	
 	public static void main(String[] args) {
-		System.out.println("2");
+		System.out.println("테스트1111111111111");
 		
 		
 		
--- CD_Inside_web/src/main/java/com/sist/cd/Test2.java
+++ CD_Inside_web/src/main/java/com/sist/cd/Test2.java
@@ -3,7 +3,7 @@ package com.sist.cd;
 public class Test2 {
 	
 	public static void main(String[] args) {
-		System.out.println("1");
+		System.out.println("테스트222222222222");
 
 		
 		
