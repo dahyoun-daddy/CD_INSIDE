fc847574fbf60a509f3f4b1939808b17f2fa2bd7
sist <sist@sist-Samsung-DeskTop-System>
2018-12-04 18:16:16 +0900

    123123

--- CD_Inside_web/src/main/java/com/sist/cd/Test.java
+++ CD_Inside_web/src/main/java/com/sist/cd/Test.java
@@ -4,8 +4,5 @@ public class Test {
 	
 	public static void main(String[] args) {
 		System.out.println("승승승승승승승승승승승승승승");
-		System.out.println("승승승승승승승승승승승승승승");
-		System.out.println("장장장장장장");
-		System.out.println("구구구구구구");
 	}
 }
