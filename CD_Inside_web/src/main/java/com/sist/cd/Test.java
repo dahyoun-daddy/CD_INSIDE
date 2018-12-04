c54583e8fb8984ba5919f220eccb6ae346bae00e
sist <sist@sist-Samsung-DeskTop-System>
2018-12-04 18:54:36 +0900

    보갓상 연시오

--- CD_Inside_web/src/main/java/com/sist/cd/Test.java
+++ CD_Inside_web/src/main/java/com/sist/cd/Test.java
@@ -3,8 +3,8 @@ package com.sist.cd;
 public class Test {
 	
 	public static void main(String[] args) {
-		System.out.println("승짱꾸!!!!!!!!!!!!!!!");
-		System.out.println("111111111111111111");
+		System.out.println("보갓상");
+		
 		
 		
 		
