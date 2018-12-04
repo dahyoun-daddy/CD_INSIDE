df8927bd4fe9c38952e0d7404afec0b5fa612c14
sist <sist@sist-Samsung-DeskTop-System>
2018-12-04 18:37:07 +0900

    ㅎㅇㅎㅇ 승짱구

--- CD_Inside_web/src/main/java/com/sist/cd/Test.java
+++ CD_Inside_web/src/main/java/com/sist/cd/Test.java
@@ -3,15 +3,8 @@ package com.sist.cd;
 public class Test {
 	
 	public static void main(String[] args) {
-		System.out.println("승승승승승승승승승승승123123승승승");
-		System.out.println("승승승승승승승승승승승123123승승승");
-		System.out.println("승승승승승승승승승승승123123승승승");
-		System.out.println("승승승승승승승승승승승123123승승승");
-		System.out.println("승승승승승승승승승승승123123승승승");
-		System.out.println("승승승승승승승승승승승123123승승승");
-		System.out.println("승승승승승승승승승승승123123승승승");
-		System.out.println("승승승승승승승승승승승123123승승승");
-		System.out.println("승승승승승승승승승승승123123승승승");
+		System.out.println("승짱꾸!!!!!!!!!!!!!!!");
+		
 		
 		
 		
