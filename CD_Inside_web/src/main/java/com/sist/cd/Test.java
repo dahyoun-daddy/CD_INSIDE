209c00f31a4c9db12bbbaaa8d94e1c28f30d4623
sist <sist@sist-Samsung-DeskTop-System>
2018-12-04 18:12:28 +0900

    승장구 추가

--- CD_Inside_web/src/main/java/com/sist/cd/Test.java
+++ CD_Inside_web/src/main/java/com/sist/cd/Test.java
@@ -4,5 +4,8 @@ public class Test {
 	
 	public static void main(String[] args) {
 		System.out.println("승승승승승승승승승승승승승승");
+		System.out.println("승승승승승승승승승승승승승승");
+		System.out.println("장장장장장장");
+		System.out.println("구구구구구구");
 	}
 }
