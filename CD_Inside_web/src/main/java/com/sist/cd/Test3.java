07987c7c9037f7ce529e95190ed4baf018a4d840
sist <sist@sist-Samsung-DeskTop-System>
2018-12-04 19:30:40 +0900

    213124124123412

---
+++ CD_Inside_web/src/main/java/com/sist/cd/Test3.java
@@ -0,0 +1,12 @@
+package com.sist.cd;
+
+public class Test3 {
+	
+	public static void main(String[] args) {
+		System.out.println(" 바론 연진 !!");
+		
+		
+		
+		
+	}
+}
---
+++ CD_Inside_web/src/main/java/com/sist/cd/Test4.java
@@ -0,0 +1,12 @@
+package com.sist.cd;
+
+public class Test4 {
+	
+	public static void main(String[] args) {
+		System.out.println(" 바론 연진 !!");
+		
+		
+		
+		
+	}
+}
