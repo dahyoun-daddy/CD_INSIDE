fae870ddb860df4501839ed49cf6bfc403c32377
suna3594 <suna3594@naver.com>
2018-12-12 17:07:07 +0900

    ignore 주석했음

--- CD_Inside_web/src/test/java/com/sist/cd/MsgDAOTest.java
+++ CD_Inside_web/src/test/java/com/sist/cd/MsgDAOTest.java
@@ -72,7 +72,7 @@ public class MsgDAOTest {
 		LOG.info("msgDao:"+msgDao);
 	}
 	
-	@Ignore
+//	@Ignore
 	@Test
 	public void get() throws Exception {
 		LOG.info("1==========================");
@@ -90,7 +90,7 @@ public class MsgDAOTest {
 		
 	}
 	
-	@Ignore
+//	@Ignore
 	@Test
 	public void update() throws SQLException, ClassNotFoundException {
 		//--------------------------------------------
@@ -101,7 +101,7 @@ public class MsgDAOTest {
 
 	}
 	
-	@Ignore
+//	@Ignore
 	@Test
 	public void delete() throws SQLException, ClassNotFoundException {
 		//--------------------------------------------
@@ -126,7 +126,7 @@ public class MsgDAOTest {
 		msgDao.deleteN("받는이3");  //됨		
 	}
 
-	@Ignore	
+//	@Ignore	
 	@Test
 	public void add() throws SQLException, ClassNotFoundException {
 		//--------------------------------------------
@@ -142,7 +142,7 @@ public class MsgDAOTest {
 
 	}	
 	
-	@Ignore
+//	@Ignore
 	@Test	
 	public void count() throws SQLException, ClassNotFoundException {
 		msgDao.add(inVO2); 
@@ -157,8 +157,9 @@ public class MsgDAOTest {
 		//--------------------------------------------
 		assertThat(msgDao.getNCount("받는이2"),is(1)); //받는이2 기준, 안읽은쪽지 갯수 
 
-	}		
+	}	
 	
+//	@Ignore		
 	@Test
 	public void addAndGet() throws SQLException, ClassNotFoundException {
 	
@@ -186,6 +187,7 @@ public class MsgDAOTest {
 
 	}
 	
+//	@Ignore		
 	@Test
 	public void do_retrieve() throws SQLException, ClassNotFoundException {
 		List<MsgVO> list = msgDao.do_retrieve(searchVO);
